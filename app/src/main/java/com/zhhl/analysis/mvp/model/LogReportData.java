2044c032f8f777def58447acdd70b9d722f4e99e
andromedaX7 <andromeda7@126.com>
2019-05-23 19:20:38 +0800

    Initial commit

---
+++ app/src/main/java/com/zhhl/analysis/mvp/model/LogReportData.java
@@ -0,0 +1,146 @@
+package com.zhhl.analysis.mvp.model;
+
+public class LogReportData {
+
+
+    private String destIp;
+    private String logType;
+    private String formatParam;
+    private String module;
+    private String sessionId;
+
+    public String getDestIp() {
+        return destIp;
+    }
+
+    public void setDestIp(String destIp) {
+        this.destIp = destIp;
+    }
+
+    public String getLogType() {
+        return logType;
+    }
+
+    public void setLogType(String logType) {
+        this.logType = logType;
+    }
+
+    public String getFormatParam() {
+        return formatParam;
+    }
+
+    public void setFormatParam(String formatParam) {
+        this.formatParam = formatParam;
+    }
+
+    public String getModule() {
+        return module;
+    }
+
+    public void setModule(String module) {
+        this.module = module;
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getCardNo() {
+        return cardNo;
+    }
+
+    public void setCardNo(String cardNo) {
+        this.cardNo = cardNo;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String getResponseType() {
+        return responseType;
+    }
+
+    public void setResponseType(String responseType) {
+        this.responseType = responseType;
+    }
+
+    public String getDestPort() {
+        return destPort;
+    }
+
+    public void setDestPort(String destPort) {
+        this.destPort = destPort;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+
+    public String getTerminalIp() {
+        return terminalIp;
+    }
+
+    public void setTerminalIp(String terminalIp) {
+        this.terminalIp = terminalIp;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public LogReportData() {
+    }
+
+    public LogReportData(String destIp, String logType, String formatParam, String module, String sessionId, String source, String cardNo, String result, String responseType, String destPort, String response, String terminalIp, String time) {
+        this.destIp = destIp;
+        this.logType = logType;
+        this.formatParam = formatParam;
+        this.module = module;
+        this.sessionId = sessionId;
+        this.source = source;
+        this.cardNo = cardNo;
+        this.result = result;
+        this.responseType = responseType;
+        this.destPort = destPort;
+        this.response = response;
+        this.terminalIp = terminalIp;
+        this.time = time;
+    }
+
+    private String source;
+    private String cardNo;
+    private String result;
+    private String responseType;;
+
+    private String destPort;
+    private String response;
+    private String terminalIp;
+    private String time;
+
+
+}
---
+++ app/src/main/java/com/zhhl/analysis/net/proxy/ILogUploadImpl.java
@@ -0,0 +1,101 @@
+package com.zhhl.analysis.net.proxy;
+
+import android.os.Process;
+import android.util.Log;
+
+import com.xdja.sslvpn.api.VpnApi50;
+import com.zhhl.analysis.app.App;
+import com.zhhl.analysis.data.LoginBean;
+import com.zhhl.analysis.mvp.model.LogReportData;
+import com.zhhl.analysis.net.HttpTools;
+import com.zhhl.analysis.utils.MacUtils;
+
+import io.reactivex.Observable;
+import io.reactivex.schedulers.Schedulers;
+
+public class ILogUploadImpl {
+    private ILogUpload iLogUpload;
+    private VpnApi50 vpnApi50;
+
+    public ILogUploadImpl(VpnApi50 vpnApi50) {
+        this.iLogUpload = HttpTools.build(ILogUpload.class, "http://192.168.20.228:7103/");//"http://192.168.20.228:7122/"
+        this.vpnApi50 = vpnApi50;
+    }
+//    logType
+//    formatParam
+//    sessionId
+//    source
+//    module
+//    response
+//    cardNo
+//    responseType
+//    result
+//    time
+//    terminalIp
+
+
+    public void log(String params, String logType, String response) {
+        LoginBean.UserInfoBean userInfo = App.app().getUserInfo().getUserInfo();
+        Log.e("log: ", "pid :" + Process.myPid());
+        /**
+         * String policeId,
+         *  String sn,
+         *  String cardNo,
+         *  String logType,
+         *  String params,
+         *  String url,
+         *  String terminalIp,
+         *  String module,
+         *  String formatParam,
+         *  String response,
+         *  String responseTime,
+         *  String sourceIp,
+         *  String sourcePort
+         */
+        if (App.app().getUserInfo() != null)
+            if (userInfo != null)//java.security.NoSuchAlgorithmException: The BC provider no longer provides an implementation for CertificateFactory.X.509
+
+
+                upload("192.168.20.228",logType,params,"人车轨迹分析",System.currentTimeMillis()+"-"+userInfo.getCode(),
+                        "107",userInfo.getIdentifier() ,"成功","1","7103",response, MacUtils.getIPAddress(App.app()),String .valueOf(System.currentTimeMillis()))
+//                upload(userInfo.getCode(), ""/*vpnApi50.getDefaultCertSN()*/, userInfo.getIdentifier(),
+//                        "12", params, url, MacUtils.getIPAddress(App.app()),
+//                        "", "", response, "500", "192.168.20.228",
+//                        "7103")
+                        .observeOn(Schedulers.io())
+                        .subscribeOn(Schedulers.computation())
+                        .subscribe(this::logres, this::err, this::complete)
+                        .isDisposed();
+    }
+
+    Observable<Object> upload(String destIp,
+                              String logType,
+                              String formatParam,
+                              String module,
+                              String sessionId,
+                              String source,
+                              String cardNo,
+                              String result,
+                              String responseType,
+                              String destPort,
+                              String response,
+                              String terminalIp,
+                              String time
+    ) {
+        //String destIp, String logType, String formatParam, String module, String sessionId, String source, String cardNo, String result, String responseType, String destPort, String response, String terminalIp, String terminalIp
+        LogReportData logReportData = new LogReportData(destIp, logType, formatParam, module, sessionId, source, cardNo, result, responseType, destPort, response, terminalIp, time);
+        return iLogUpload.upload((logReportData));
+    }
+
+    private void complete() {
+
+    }
+
+    private void err(Throwable throwable) {
+        throwable.printStackTrace();
+    }
+
+    private void logres(Object o) {
+        Log.e("logReport>>>: ", o.toString());
+    }
+}
